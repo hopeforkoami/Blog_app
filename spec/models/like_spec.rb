2fbfe090626bf01c5124024c39a166088c6dfe06
hopeforkoami <hopeforkoami@gmail.com>
2023-03-05 02:14:27 +0000

    Add test for like

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../rails_helper'
+
+RSpec.describe Like, type: :model do
+  let(:user) { User.create(name: 'Koami NOGBEDJI', photo:'https://linktomyphoto.com/koami_profil', bio: 'my self') }
+  let(:post) { Post.create(title: 'post title', text: 'this is a text for the post', comments_counter: 20, likes_counter: 30,
+                           author_id: user.id) }
+  subject { Like.new(author_id: user.id, post_id: post.id) }
+  
+  before { subject.save }
+
+  it 'author should be present' do
+    subject.user = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a post' do
+    subject.post = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'increments the likes_counter of the associated post after saving' do
+    expect { subject.save }.to change { post.reload.likes_counter }.by(1)
+  end
+
+end
