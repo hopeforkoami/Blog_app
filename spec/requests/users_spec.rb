3e095c5c191e0c68a86ef5168baffba282392ea8
hopeforkoami <hopeforkoami@gmail.com>
2023-03-14 14:31:31 +0000

    Add test for the user show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'UsersController', type: :request do
 
   before{ subject.save }
 
-  describe ' GEt #index' do
+  describe ' GET #index' do
     before(:example) { get users_path } # get(:index)
     it 'returns http success' do
       get users_path
@@ -16,6 +16,19 @@ RSpec.describe 'UsersController', type: :request do
     end
     it "must include  the page body" do
       expect(response.body).to include("This is the Users index page.")
+    end
+  end
+
+  describe ' GET #show' do
+    before(:example) { get user_path(id: 3) } # get(:show)
+    it 'returns http success' do
+      expect(response).to have_http_status(200)
+    end
+    it "renders the correct template" do
+      expect(response).to render_template(:show)
+    end
+    it "must include  the page body" do
+      expect(response.body).to include("this is the User show page.")
     end
   end
 end
