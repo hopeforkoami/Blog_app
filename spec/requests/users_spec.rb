93798df5bcc41ee8975d19e195b6960eabccad40
hopeforkoami <hopeforkoami@gmail.com>
2023-03-14 14:46:40 +0000

    Correct Linter errors

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'UsersController', type: :request do
   subject { User.new(name: 'Koami NOGBEDJI', photo: 'https://linktomyphoto.com/koami_profil', bio: 'my self', posts_counter: 20) }
 
-  before{ subject.save }
+  before { subject.save }
 
   describe ' GET #index' do
     before(:example) { get users_path } # get(:index)
@@ -11,11 +11,11 @@ RSpec.describe 'UsersController', type: :request do
       get users_path
       expect(response).to have_http_status(200)
     end
-    it "renders the correct template" do
+    it 'renders the correct template' do
       expect(response).to render_template(:index)
     end
-    it "must include  the page body" do
-      expect(response.body).to include("This is the Users index page.")
+    it 'must include  the page body' do
+      expect(response.body).to include('This is the Users index page.')
     end
   end
 
@@ -24,11 +24,11 @@ RSpec.describe 'UsersController', type: :request do
     it 'returns http success' do
       expect(response).to have_http_status(200)
     end
-    it "renders the correct template" do
+    it 'renders the correct template' do
       expect(response).to render_template(:show)
     end
-    it "must include  the page body" do
-      expect(response.body).to include("this is the User show page.")
+    it 'must include  the page body' do
+      expect(response.body).to include('this is the User show page.')
     end
   end
 end
