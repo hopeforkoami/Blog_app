597ef2d902cf297636365ce8a4d37db3a881b591
hopeforkoami <hopeforkoami@gmail.com>
2023-03-19 01:38:02 +0000

    Create comment conroller and helper

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,10 @@
+class CommentsController < ApplicationController
+  def new
+    
+  end
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to user_post_path(user_id: @post.author_id, id: @post.id)
+  end
+end
