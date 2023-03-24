c29f346fc9f331310bfa1fd7bc3f8acbfa28edd9
hopeforkoami <hopeforkoami@gmail.com>
2023-03-24 23:57:24 +0000

    Correct linter errors

--- spec/system/user_view_spec.rb
+++ spec/system/user_view_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'User view', type:feature do
-    before do
-        @user1 = User.create(name: 'Koami')
-        @user2 = User.create(name: 'Kokou')
-        @user3 = User.create(name: 'Alice')
-    end
-    describe 'Index page' do
-        it 'should show all users name' do
-            visit '/users'
+RSpec.describe 'User view', type: feature do
+  before do
+    @user1 = User.create(name: 'Koami', photo: 'https://linktomyphoto.com/koami_profil')
+    @user2 = User.create(name: 'Kokou')
+    @user3 = User.create(name: 'Alice')
+  end
+  describe 'Index page' do
+    it 'should show all users name' do
+      visit '/users'
 
-            expect(page).to have_content(@user1.name)
-            expect(page).to have_content(@user2.name)
-            expect(page).to have_content(@user3.name)
-        end
+      expect(page).to have_content(@user1.name)
+      expect(page).to have_content(@user2.name)
+      expect(page).to have_content(@user3.name)
     end
+
+    it 'should display image' do
+      visit '/users'
+
+      expect(page).to have_selector("img[src='#{@user1.photo}']")
+    end
+  end
 end
